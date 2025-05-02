755176c45abbfb32d930649b2d67227b89a10c44
Shane Gray <shane.joseph.gray@gmail.com>
2025-05-02 20:38:45 +0100

    Added shadcn

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Hello World</h1>
-		</>
+		<div className="flex flex-col items-center justify-center min-h-svh">
+			<Button>Click me</Button>
+		</div>
 	);
 }
 
